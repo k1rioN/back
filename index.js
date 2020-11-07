effd16b9a83fce8a514bb13247588f915e961eb5
k1rioN <58228299+k1rioN@users.noreply.github.com>
2020-11-07 13:52:42 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+const faunadb = require('faunadb');
+const express = require('express')
+const app = express()
+const port = 3001
+const fs = require('fs')
+
+const q = faunadb.query;
+const client = new faunadb.Client({ secret: 'fnAD5_no-RACBfEBngVhP0ZFtlQM1RDOqINAqU71' });
+const {
+  Map,
+  Paginate,
+  Collection,
+  Lambda,
+  Get,
+  Var,
+  Index,
+  Match,
+  Update,
+  Ref,
+  Delete,
+  Create
+} = q;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/api/importactor', function(req,res) {
+  let rawdata = fs.readFileSync('film.imdb.json');
+  let student = JSON.parse(rawdata);
+  console.log(student);
+  client
+  .query(
+    Create(
+      Collection('films'),
+      { data: { student } },
+    )
+  )
+  res.send("Post done!")
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
