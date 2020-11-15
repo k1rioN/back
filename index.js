ab2e5434b88d57878d383b4f0111353722ac4e43
k1rioN <58228299+k1rioN@users.noreply.github.com>
2020-11-15 22:18:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const port = 3001
 const fs = require('fs')
+var cors = require('cors')
+
+app.use(cors())
 
 const q = faunadb.query;
 const client = new faunadb.Client({ secret: 'fnAD5_no-RACBfEBngVhP0ZFtlQM1RDOqINAqU71' });
@@ -25,7 +28,13 @@ const {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
+module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -38,9 +47,7 @@ app.post('/api/importactor', function(req,res) {
   client
   .query(
     Map(
-      [
-        student
-      ],
+        student,
       Lambda(
         'post_title',
         Create(
@@ -51,6 +58,22 @@ app.post('/api/importactor', function(req,res) {
     )
   )
   res.send("Post done!")
+})
+
+app.get("/api/filmlist", function(req,res) {
+  client
+  .query(Map(
+    Paginate(
+      Match(
+        Index("1")
+        )
+        ),
+        Lambda("Title", Get(Var("Title")))
+        ))
+    .then(result => {
+      res.send(result)
+    })
+    console.log("ко мне обратились")
 })
 
 
